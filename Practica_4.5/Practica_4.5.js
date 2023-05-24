215b272918b62691a6ac0ab81949134f6569e9a0
Albert <albertlb08@gmail.com>
2023-05-24 21:55:42 +0200

    Progress I think

--- Practica_4.5/Practica_4.5.js
+++ Practica_4.5/Practica_4.5.js
@@ -1,3 +1,20 @@
 function myAnimeList() {
+    fetch('https://api.jikan.moe/v4/random/anime')
+    .then(response => response.json()) // Convertir la respuesta a JSON
+    .then(data => {
+      // Formatear y mostrar la información del anime
+      const tituloAnime = data.title;
+      const sinopsisAnime = data.synopsis;
+
+      const elementoTitulo = document.getElementById('titulo1');
+      elementoTitulo.textContent = tituloAnime;
+
+      const elementoSinopsis = document.getElementById('textoSinopsis');
+      elementoSinopsis.textContent = sinopsisAnime;
+      
+    })
+    .catch(error => {
+      console.error('Error al obtener la información del anime:', error);
+    });
     
 }
